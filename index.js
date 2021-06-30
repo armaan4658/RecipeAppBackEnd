f9701c190a5a226d60a0ebdb7994d6a21107cc3d
armaan4658 <armaan4658@gmail.com>
2021-06-30 18:32:21 +0530

    removed cart data

--- index.js
+++ index.js
@@ -5,55 +5,7 @@ import cors from "cors";
 import {Recipe} from './models/users.js';
 const app = express();
 const PORT = 5000;
-const USERS = [
-    {
-        "product_name":"Fancy Product",
-        "product_price":"$40.00-$80.00",
-        "img":"https://cdn5.vectorstock.com/i/1000x1000/62/89/black-shopping-cart-icon-isolated-on-yellow-vector-33126289.jpg"
-    },
-    {
-        "product_name":"Popular Item",
-        "product_price":"$40.00",
-        "img":"https://cdn5.vectorstock.com/i/1000x1000/62/89/black-shopping-cart-icon-isolated-on-yellow-vector-33126289.jpg"
-    },
-    {
-        "product_name":"Fancy Product",
-        "product_price":"$40.00-$80.00",
-        "img":"https://cdn5.vectorstock.com/i/1000x1000/62/89/black-shopping-cart-icon-isolated-on-yellow-vector-33126289.jpg"
-    },
-    {
-        "product_name":"Sale Item",
-        "product_price":"$25.00",
-        "img":"https://cdn5.vectorstock.com/i/1000x1000/62/89/black-shopping-cart-icon-isolated-on-yellow-vector-33126289.jpg"
-    },
-    {
-        "product_name":"Popular Item",
-        "product_price":"$40.00",
-        "img":"https://cdn5.vectorstock.com/i/1000x1000/62/89/black-shopping-cart-icon-isolated-on-yellow-vector-33126289.jpg"
-    },
-    {
-        "product_name":"Fancy Product",
-        "product_price":"$40.00-$80.00",
-        "img":"https://cdn5.vectorstock.com/i/1000x1000/62/89/black-shopping-cart-icon-isolated-on-yellow-vector-33126289.jpg"
-    },
-    {
-        "product_name":"Sale Item",
-        "product_price":"$25.00",
-        "img":"https://cdn5.vectorstock.com/i/1000x1000/62/89/black-shopping-cart-icon-isolated-on-yellow-vector-33126289.jpg"
-    },
-    {
-        "product_name":"Fancy Product",
-        "product_price":"$40.00-$80.00",
-        "img":"https://cdn5.vectorstock.com/i/1000x1000/62/89/black-shopping-cart-icon-isolated-on-yellow-vector-33126289.jpg"
-    },
-    {
-        "product_name":"Popular Item",
-        "product_price":"$40.00",
-        "img":"https://cdn5.vectorstock.com/i/1000x1000/62/89/black-shopping-cart-icon-isolated-on-yellow-vector-33126289.jpg"
-    }
-]
 //Data base url
-
 //const url ="mongodb+srv://armaan:guvi123456@cluster0.qx8hr.mongodb.net/recipeApp?retryWrites=true&w=majority";
 const url ="mongodb://localhost/recipeApp";
 mongoose.connect(url,{useNewUrlParser:true});
@@ -63,20 +15,20 @@ con.on('open',()=>console.log('MongoDB is connected'));
 //Creating middleware
 app.use(express.json());
 //implementing cors
-// var allowedOrigins = ['http://localhost:3000'];
-// app.use(cors({
-//   origin: function(origin, callback){
-//     // allow requests with no origin 
-//     // (like mobile apps or curl requests)
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){
-//       var msg = 'The CORS policy for this site does not ' +
-//                 'allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+var allowedOrigins = ['http://localhost:3000'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.get('/',(request,response)=>{
     response.send('Welcome to postman');
 })
@@ -110,7 +62,7 @@ app.delete('/recipes/:id',async(request,response)=>{
     response.send(user);
     console.log(user);
 })
-app.patch('/recipes/:id',async(request,response)=>{
+app.put('/recipes/:id',async(request,response)=>{
     const update = request.body;
     const {id} = request.params;
     try{
